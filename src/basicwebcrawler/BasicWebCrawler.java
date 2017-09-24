b52cc1e677fc0e62e55ac40ac41ac922e78dd3f7
cipher <cipher@localhost>
2017-09-24 22:02:27 +0800

    # WARNING: head commit changed in the meantime
    Change-Id: I505a9d81827184f4e46b070e4ef40c58a50cef75
    Signed-off-by: cipher <cipher@localhost>

---
+++ src/basicwebcrawler/BasicWebCrawler.java
@@ -0,0 +1,47 @@
+package basicwebcrawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.lang.IllegalArgumentException;
+
+
+public class BasicWebCrawler {
+
+	private HashSet<String> links;
+	
+	public BasicWebCrawler(){
+		links=new  HashSet<String>();
+	}
+	
+	public void getPageLinks(String URL){
+		if(!links.contains(URL)){
+			try{
+				if(links.add(URL)){
+					System.out.println(URL);
+				}
+				Document document=Jsoup.connect(URL).get();
+				Elements linksOnPage=document.select("a[href]");
+				
+				for(Element page:linksOnPage){
+					getPageLinks(page.attr("abs:href"));
+				}
+			}
+			catch(IOException e){
+				System.err.println("For '"+URL+"': "+e.getMessage());
+			}
+			catch(IllegalArgumentException ilge){
+				System.err.println("For '"+URL+"': "+ilge.getMessage());
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+        //1. Pick a URL from the frontier
+        new BasicWebCrawler().getPageLinks("http://www.mkyong.com/");
+    }
+}
